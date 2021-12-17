067173c8db5111bf03dae65659eaed7d323442d3
michael gunawan <phuaan@yahoo.com>
2021-12-17 22:56:29 +0700

    add vial support(tested)

---
+++ keyboards/slepet/keymaps/vial/config.h
@@ -0,0 +1,23 @@
+/*
+Copyright 2021 Michael Gunawan
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xBD, 0xF6, 0xC1, 0x89, 0x6F, 0xB8, 0x2C, 0x15}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2, 4, 6 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0, 0, 0 }
