312046d383c9955f41ad78d1df555633db88a20d
michael gunawan <phuaan@yahoo.com>
2021-12-21 18:08:30 +0700

    add combo and increase tap dan count

--- keyboards/slepet/keymaps/vial/config.h
+++ keyboards/slepet/keymaps/vial/config.h
@@ -21,3 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 2, 4, 6 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 0, 0, 0 }
+
+#define VIAL_TAP_DANCE_ENTRIES 20
+#define VIAL_COMBO_ENTRIES 20
